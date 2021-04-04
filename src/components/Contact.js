8993c8b330d6f38c3dcbeabea8ee23d3a97d922d
Matt Verity <mdverity@gmail.com>
2021-04-03 22:41:13 -0400

    added scroll transitions for sections.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Form, Button, Row, Col, Container } from 'react-bootstrap'
 import styled from 'styled-components'
 import contactImg from '../assets/images/contact.jpg'
 import useForm from '../hooks/useForm'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 const Contact = () => {
+  const animOne = useAnimation()
+
+  const animTwo = useAnimation()
+
+  const [formRef, formView] = useInView({
+    triggerOnce: true,
+    rootMargin: '-150px',
+  })
+
+  const [mapRef, mapView] = useInView({
+    triggerOnce: true,
+    rootMargin: '0px',
+  })
+
+  useEffect(() => {
+    if (formView) {
+      animOne.start('visible')
+    }
+  }, [animOne, formView])
+
+  useEffect(() => {
+    if (mapView) {
+      animTwo.start('visible')
+    }
+  }, [animTwo, mapView])
+
   const { handleChange, values, handleSubmit } = useForm()
 
   const { REACT_APP_GMAPS_KEY } = process.env
@@ -29,113 +57,168 @@ const Contact = () => {
       </Row>
       <Row>
         <Col md={7} className='m-auto'>
-          <ContactImage src={contactImg} alt='contact' />
+          <motion.div
+            id='showcase'
+            ref={formRef}
+            animate={animOne}
+            initial='hidden'
+            variants={{
+              visible: {
+                opacity: 1,
+                x: 0,
+                transition: { duration: 1.2, ease: [0.6, 0.05, -0.01, 0.9] },
+              },
+              hidden: { opacity: 0, x: -150 },
+            }}
+          >
+            <ContactImage src={contactImg} alt='contact' />
+          </motion.div>
         </Col>
         <Col md={5} className='m-md-auto mt-2'>
-          {/* <Card className='p-2' bg='light'> */}
-          <Form name='contact' data-netlify='true' onSubmit={handleSubmit}>
-            <Form.Group className='d-flex flex-column' controlId='formEmail'>
-              <Form.Control
-                name='email'
-                type='email'
-                className='text-center mt-2'
-                value={values.email}
-                onChange={handleChange}
-                placeholder='Your Email Address'
-                style={{
-                  resize: 'none',
-                  backgroundColor: 'transparent',
-                  border: 'none',
-                  borderRadius: '0',
-                  borderBottom: '1px solid #7a7a7a',
-                }}
-              />
-              <Form.Text className='text-muted mx-auto'>
-                <small>We'll never share your email with anyone else.</small>
-              </Form.Text>
-            </Form.Group>
-            <Form.Group controlId='formMessage'>
-              <Form.Control
-                as='textarea'
-                name='message'
-                className='text-center'
-                placeholder='Your Message'
-                value={values.message}
-                onChange={handleChange}
-                rows={3}
-                style={{
-                  resize: 'none',
-                  backgroundColor: 'transparent',
-                  border: 'none',
-                  borderRadius: '0',
-                  // paddingTop: '1rem',
-                  borderBottom: '1px solid #7a7a7a',
-                }}
-              />
-            </Form.Group>
-            <Form.Group
-              name='help-with'
-              controlId='formBasicCheckbox'
-              className='d-flex flex-column justify-items-center'
-            >
-              <Form.Label className='text-center'>
-                What can we help you with?
-              </Form.Label>
-              <div style={{ width: 'fit-content', margin: '0 auto' }}>
-                <Form.Check
-                  className='text-dark'
-                  type='checkbox'
-                  label='Services'
-                  name='services'
-                  checked={values.services}
+          <motion.div
+            ref={formRef}
+            animate={animOne}
+            initial='hidden'
+            variants={{
+              visible: {
+                opacity: 1,
+                x: 0,
+                transition: { duration: 1.2, ease: [0.6, 0.05, -0.01, 0.9] },
+              },
+              hidden: { opacity: 0, x: 150 },
+            }}
+          >
+            {/* <Card className='p-2' bg='light'> */}
+            <Form name='contact' data-netlify='true' onSubmit={handleSubmit}>
+              <Form.Group className='d-flex flex-column' controlId='formEmail'>
+                <Form.Control
+                  name='email'
+                  type='email'
+                  className='text-center mt-2'
+                  value={values.email}
                   onChange={handleChange}
+                  placeholder='Your Email Address'
+                  style={{
+                    resize: 'none',
+                    backgroundColor: 'transparent',
+                    border: 'none',
+                    borderRadius: '0',
+                    borderBottom: '1px solid #7a7a7a',
+                  }}
                 />
-                <Form.Check
-                  className='text-dark'
-                  type='checkbox'
-                  label='Estimate'
-                  name='estimate'
-                  checked={values.estimate}
+                <Form.Text className='text-muted mx-auto'>
+                  <small>We'll never share your email with anyone else.</small>
+                </Form.Text>
+              </Form.Group>
+              <Form.Group controlId='formMessage'>
+                <Form.Control
+                  as='textarea'
+                  name='message'
+                  className='text-center'
+                  placeholder='Your Message'
+                  value={values.message}
                   onChange={handleChange}
+                  rows={3}
+                  style={{
+                    resize: 'none',
+                    backgroundColor: 'transparent',
+                    border: 'none',
+                    borderRadius: '0',
+                    // paddingTop: '1rem',
+                    borderBottom: '1px solid #7a7a7a',
+                  }}
                 />
-                <Form.Check
-                  className='text-dark'
-                  type='checkbox'
-                  label='Products'
-                  name='products'
-                  checked={values.products}
-                  onChange={handleChange}
-                />
-                <Form.Check
-                  className='text-dark'
-                  type='checkbox'
-                  label='Consultation'
-                  name='consultation'
-                  checked={values.consultation}
-                  onChange={handleChange}
-                />
-              </div>
-            </Form.Group>
-            <Button className='w-100 mx-auto my-2' variant='dark' type='submit'>
-              Submit
-            </Button>
-          </Form>
+              </Form.Group>
+              <Form.Group
+                name='help-with'
+                controlId='formBasicCheckbox'
+                className='d-flex flex-column justify-items-center'
+              >
+                <Form.Label className='text-center'>
+                  What can we help you with?
+                </Form.Label>
+                <div style={{ width: 'fit-content', margin: '0 auto' }}>
+                  <Form.Check
+                    className='text-dark'
+                    type='checkbox'
+                    label='Services'
+                    name='services'
+                    checked={values.services}
+                    onChange={handleChange}
+                  />
+                  <Form.Check
+                    className='text-dark'
+                    type='checkbox'
+                    label='Estimate'
+                    name='estimate'
+                    checked={values.estimate}
+                    onChange={handleChange}
+                  />
+                  <Form.Check
+                    className='text-dark'
+                    type='checkbox'
+                    label='Products'
+                    name='products'
+                    checked={values.products}
+                    onChange={handleChange}
+                  />
+                  <Form.Check
+                    className='text-dark'
+                    type='checkbox'
+                    label='Consultation'
+                    name='consultation'
+                    checked={values.consultation}
+                    onChange={handleChange}
+                  />
+                </div>
+              </Form.Group>
+              <Button
+                className='w-100 mx-auto my-2'
+                variant='dark'
+                type='submit'
+              >
+                Submit
+              </Button>
+            </Form>
 
-          {/* </Card> */}
+            {/* </Card> */}
+          </motion.div>
         </Col>
       </Row>
-      <div style={{ display: 'grid', placeItems: 'center', marginTop: '4rem' }}>
+      <motion.div
+        ref={mapRef}
+        animate={animTwo}
+        initial='hidden'
+        variants={{
+          visible: {
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1.2, ease: [0.6, 0.05, -0.01, 0.9] },
+          },
+          hidden: { opacity: 0, y: 150 },
+        }}
+        style={{
+          display: 'grid',
+          placeItems: 'center',
+          marginTop: '4rem',
+          height: '400px',
+        }}
+      >
         <iframe
           title='google-maps'
-          width='600'
-          height='400'
           loading='lazy'
-          style={{ border: '1px solid rgba(100,100,100,0.5)' }}
+          style={{
+            border: '1px solid rgba(100,100,100,0.5)',
+            maxWidth: '600px',
+            maxHeight: '400px',
+            width: '100%',
+            height: '100%',
+          }}
           allowFullScreen
           className='mx-auto'
           src={gMapsAPI}
         ></iframe>
-      </div>
+      </motion.div>
     </Container>
   )
 }
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,82 +1,174 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import products1 from '../assets/images/products1small.jpg'
 import products2 from '../assets/images/products2small.jpg'
 import products3 from '../assets/images/products3small.jpg'
 import styled from 'styled-components'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 const Showcase = () => {
+  const animation1 = useAnimation()
+
+  const animation2 = useAnimation()
+
+  const animation3 = useAnimation()
+
+  const [itemOne, itemOneView] = useInView({
+    triggerOnce: true,
+    rootMargin: '150px',
+  })
+
+  const [itemTwo, itemTwoView] = useInView({
+    triggerOnce: true,
+    rootMargin: '150px',
+  })
+
+  const [itemThree, itemThreeView] = useInView({
+    triggerOnce: true,
+    rootMargin: '150px',
+  })
+
+  useEffect(() => {
+    if (itemOneView) {
+      animation1.start('visible')
+    }
+  }, [animation1, itemOneView])
+
+  useEffect(() => {
+    if (itemTwoView) {
+      animation2.start('visible')
+    }
+  }, [animation2, itemTwoView])
+
+  useEffect(() => {
+    if (itemThreeView) {
+      animation3.start('visible')
+    }
+  }, [animation3, itemThreeView])
+
   return (
     <Container id='showcase'>
-      <Row>
-        <Col md={5} className='d-flex order-md-last'>
-          <ProductImage src={products1} className='ml-auto' alt='product1' />
-        </Col>
-        <Col md={1} />
-        <Col md={6} className='align-self-center order-md-first'>
-          <div>
-            <h2
-              variant='h2'
-              className='h2 mt-md-0 mt-2'
-              style={{ color: '#303030' }}
-            >
-              Here's a Headline
-            </h2>
-            <p variant='p' style={{ color: '#3d3d3d' }}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis
-              culpa praesentium ullam qui illum laudantium, quibusdam quas
-              commodi iure deserunt.
-            </p>
-          </div>
-        </Col>
-      </Row>
+      <motion.div
+        id='showcase'
+        ref={itemOne}
+        animate={animation1}
+        initial='hidden'
+        variants={{
+          visible: {
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1.2, ease: [0.6, 0.05, -0.01, 0.9] },
+          },
+          hidden: { opacity: 0, y: 150 },
+        }}
+        className='mx-auto'
+        style={{ width: '90%' }}
+      >
+        <Row>
+          <Col md={5} className='d-flex order-md-last'>
+            <ProductImage src={products1} className='ml-auto' alt='product1' />
+          </Col>
+          <Col md={1} />
+          <Col md={6} className='align-self-center order-md-first'>
+            <div>
+              <h2
+                variant='h2'
+                className='h2 mt-md-0 mt-2'
+                style={{ color: '#303030' }}
+              >
+                Here's a Headline
+              </h2>
+              <p variant='p' style={{ color: '#3d3d3d' }}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Debitis culpa praesentium ullam qui illum laudantium, quibusdam
+                quas commodi iure deserunt.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </motion.div>
       <SpacerSM />
-      <Row>
-        <Col md={5} className='order-md-first'>
-          <ProductImage src={products2} alt='product2' />
-        </Col>
-        <Col md={1} />
-        <Col md={6} className='align-self-center order-md-last'>
-          <div>
-            <h2
-              variant='h2'
-              className='h2 mt-md-0 mt-2'
-              style={{ color: '#303030' }}
-            >
-              This is a Title
-            </h2>
-            <p variant='p' style={{ color: '#3d3d3d' }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-              distinctio soluta impedit, quasi natus repellat. Ipsum, sint odio
-              necessitatibus, enim ipsa hic voluptatibus quis consequatur
-              voluptatem iste quam rerum placeat?
-            </p>
-          </div>
-        </Col>
-      </Row>
+      <motion.div
+        id='showcase'
+        ref={itemTwo}
+        animate={animation2}
+        initial='hidden'
+        variants={{
+          visible: {
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1.2, ease: [0.6, 0.05, -0.01, 0.9] },
+          },
+          hidden: { opacity: 0, y: 150 },
+        }}
+        className='mx-auto'
+        style={{ width: '90%' }}
+      >
+        <Row>
+          <Col md={5} className='order-md-first'>
+            <ProductImage src={products2} alt='product2' />
+          </Col>
+          <Col md={1} />
+          <Col md={6} className='align-self-center order-md-last'>
+            <div>
+              <h2
+                variant='h2'
+                className='h2 mt-md-0 mt-2'
+                style={{ color: '#303030' }}
+              >
+                This is a Title
+              </h2>
+              <p variant='p' style={{ color: '#3d3d3d' }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+                distinctio soluta impedit, quasi natus repellat. Ipsum, sint
+                odio necessitatibus, enim ipsa hic voluptatibus quis consequatur
+                voluptatem iste quam rerum placeat?
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </motion.div>
       <SpacerSM />
-      <Row>
-        <Col md={5} className='d-flex order-md-last'>
-          <ProductImage src={products3} className='ml-auto' alt='product1' />
-        </Col>
-        <Col md={1} />
-        <Col md={6} className='align-self-center order-md-first'>
-          <div>
-            <h2
-              variant='h2'
-              className='h2 mt-md-0 mt-2'
-              style={{ color: '#303030' }}
-            >
-              We Design Things
-            </h2>
-            <p variant='p' style={{ color: '#3d3d3d' }}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
-              labore error tempora harum sapiente voluptates sunt et ducimus
-              consequatur corrupti.
-            </p>
-          </div>
-        </Col>
-      </Row>
+      <motion.div
+        id='showcase'
+        ref={itemThree}
+        animate={animation3}
+        initial='hidden'
+        variants={{
+          visible: {
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1.2, ease: [0.6, 0.05, -0.01, 0.9] },
+          },
+          hidden: { opacity: 0, y: 150 },
+        }}
+        className='mx-auto'
+        style={{ width: '90%' }}
+      >
+        <Row>
+          <Col md={5} className='d-flex order-md-last'>
+            <ProductImage src={products3} className='ml-auto' alt='product1' />
+          </Col>
+          <Col md={1} />
+          <Col md={6} className='align-self-center order-md-first'>
+            <div>
+              <h2
+                variant='h2'
+                className='h2 mt-md-0 mt-2'
+                style={{ color: '#303030' }}
+              >
+                We Design Things
+              </h2>
+              <p variant='p' style={{ color: '#3d3d3d' }}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+                labore error tempora harum sapiente voluptates sunt et ducimus
+                consequatur corrupti.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </motion.div>
     </Container>
   )
 }
